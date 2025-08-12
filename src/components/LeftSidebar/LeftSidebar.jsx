b11adef9dba570649b6e23a3872076afa102645a
pawan <pawanku28022004@gmail.com>
2025-08-13 01:32:10 +0530

    add kebab menu

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -10,6 +10,12 @@ const LeftSidebar = () => {
           <img src={assets.logo} className="logo" alt="" />
           <div className="menu">
             <img src={assets.menu_icon} alt="" />
+
+            <div className="sub_menu">
+              <p>Edit Profile</p>
+              <hr />
+              <p>Logout</p>
+            </div>
           </div>
         </div>
         <div className="ls_search">
