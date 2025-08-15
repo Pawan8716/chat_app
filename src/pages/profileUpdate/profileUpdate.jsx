64b63d5ef5ee02cc41ac3f0e94f94614bc50e6e5
pawan <pawanku28022004@gmail.com>
2025-08-16 01:50:44 +0530

    add profile page in chat app

--- src/pages/profileUpdate/profileUpdate.jsx
+++ src/pages/profileUpdate/profileUpdate.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./profileUpdate.css";
+import assets from "../../assets/assets";
 const ProfileUpdate = () => {
-  return <div> </div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <div className="profile">
+      <div className="profile_container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />{" "}
+            upload profile image
+          </label>
+          <input type="text" placeholder="Your name" required />
+          <textarea placeholder="Write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile_pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 export default ProfileUpdate;
