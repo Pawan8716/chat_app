162cc5bd611ff7789d01c84be26fd3a7e4cc93ae
pawan <pawanku28022004@gmail.com>
2025-08-20 01:52:19 +0530

    using firebase for backend

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7_YGJaYu1vCFlgBRFXRULh2zVV71Ms50",
+  authDomain: "chat-app-gs-b4fae.firebaseapp.com",
+  projectId: "chat-app-gs-b4fae",
+  storageBucket: "chat-app-gs-b4fae.firebasestorage.app",
+  messagingSenderId: "223952415509",
+  appId: "1:223952415509:web:959ae55a72224fbb9a171c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
